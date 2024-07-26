479b852de64cf60cf529c4c32a1093e06fafba7f
Eamon Burns (Laptop WSL) <me@eamonburns.com>
2024-07-25 17:47:18 -0700

    chore(array_list): remove debug prints

--- src/array_list.zig
+++ src/array_list.zig
@@ -15,17 +15,10 @@ pub fn ArrayList(comptime T: type) type {
         const alloc_factor = 1.25;
 
         pub fn init(init_cap: usize) !Self {
-            print("initializing...\n", .{});
-            const items = try Self.allocator.alloc(T, init_cap);
-            print("items.len: {}\n", .{items.len});
-            const cap = init_cap;
-            print("capacity: {}\n", .{cap});
-            const init_len: usize = 0;
-            print("init_len: {}\n", .{init_len});
             return Self{
-                .len = init_len,
-                .items = items,
-                .capacity = cap,
+                .len = 0,
+                .items = try Self.allocator.alloc(T, init_cap),
+                .capacity = init_cap,
             };
         }
 
@@ -39,17 +32,9 @@ pub fn ArrayList(comptime T: type) type {
         }
 
         pub fn append(self: *Self, item: T) !void {
-            print("appending {}...\n", .{item});
-            print("items.len: {}\n", .{self.items.len});
-            print("self.len: {}\n", .{self.len});
             if (self.len + 1 > self.capacity) {
-                print("allocating more space...\n", .{});
                 const new_capacity: usize = @intFromFloat(@ceil(@as(f32, @floatFromInt(self.capacity)) * Self.alloc_factor));
-                print("old capacity: {}\n", .{self.capacity});
-                print("new capacity: {}\n", .{new_capacity});
                 var new_items = try Self.allocator.alloc(T, new_capacity);
-                print("new_items.len: {}\n", .{new_items.len});
-                print("self.len: {}\n", .{self.len});
 
                 for (self.items, 0..) |old_item, i| new_items[i] = old_item;
                 self.items = new_items;
@@ -58,11 +43,6 @@ pub fn ArrayList(comptime T: type) type {
 
             self.items[self.len] = item;
             self.len += 1;
-            print("incrementing self.len: {}\n", .{self.len});
-
-            print("at the end of append {}\n", .{item});
-            //print("items.len: {}\n", .{self.items.len});
-            print("self.len: {}\n", .{self.len});
         }
 
         pub fn insertAt(self: Self, item: T, idx: usize) void {
@@ -96,11 +76,8 @@ const expect = std.testing.expect;
 test "test array list" {
     var l = try ArrayList(u8).init(3);
     try l.append(5);
-    print("l.len after 5: {}\n", .{l.len});
     try l.append(7);
-    print("l.len after 7: {}\n", .{l.len});
     try l.append(9);
-    print("l.len after 9: {}\n", .{l.len});
 
     try expect(l.get(0) == 5);
     try expect(l.get(1) == 7);
@@ -109,10 +86,8 @@ test "test array list" {
     try l.append(10);
     try expect(l.get(3) == 10);
 
-    print("capacity: {d}\n", .{l.capacity});
-    print("length: {d}\n", .{l.len});
-    print("items:\n", .{});
-    for (l.items) |item| {
-        print("{d} ", .{item});
-    }
+    //print("items:\n", .{});
+    //for (l.items) |item| {
+    //print("{d} ", .{item});
+    //}
 }
